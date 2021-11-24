34f3765b6e19b002693bd6e879b92efbbad8beaf
jjohnnyUtah <94047126+jjohnnyUtah@users.noreply.github.com>
2021-11-24 12:00:54 +0100

    Update Main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -13,19 +13,21 @@ import java.util.Vector;
 public class Main {
 	//Vector<String> v = new Vector<String>();
 	public static void main(String[] args) {
+		String line="";
 		Vector<String> company = new Vector<String>();
 		Vector<String> job = new Vector<String>();
 		Vector<String> location = new Vector<String>();
 		Vector<String> employees = new Vector<String>();
 		try {
-			Scanner pippo = new Scanner(new BufferedReader(new FileReader("file.csv")));
-			pippo.useDelimiter(","); //usa la virgola come elemento separatore
+			BufferedReader br = new BufferedReader(new FileReader("file.csv"));
 			//legge il file e lo formatta mettendo i vari token nei vetotri corrispondenti
-				while(pippo.hasNext()) {
-					company.add(pippo.next());
-					job.add(pippo.next());
-					location.add(pippo.next());
-					employees.add(pippo.next());
+				while((line = br.readLine()) != null) {
+					String[] values = line.split(","); //usa la virgola come elemento separatore
+					company.add(values[0]);
+					job.add(values[1]);
+					location.add(values[2]);
+					employees.add(values[3]);
+
 				}
 			System.out.println("%%%%%%%%%%%%%%%");
 			System.out.println(" company list: " + company);
@@ -34,7 +36,7 @@ public class Main {
 			System.out.println("num.employees: " + employees);
 			System.out.println("%%%%%%%%%%%%%%%");
 			
-			pippo.close();
+			br.close();
 		} catch(IOException e) {
 			System.out.println("Errore");
 			System.out.println(e);
